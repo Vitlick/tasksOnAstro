f0c44cb56da12d45d7b3bbdf60f7b1029caf241c
vital <vitlick@yandex.ru>
2026-01-04 10:18:21 +0500

    exclude minify html and  js

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    compressHTML:false,
+    devToolbar:{
+        enabled:false
+    },
+    vite:{
+        build:{
+            minify:false,
+            cssMinify:true
+        }
+    }
+  
+});
