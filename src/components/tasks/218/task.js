11b5623226073b75c0b7531c7d8864f7588390a7
vital <vitlick@yandex.ru>
2026-01-04 10:22:35 +0500

    task  218 is ready

--- src/components/tasks/218/task.js
+++ src/components/tasks/218/task.js
@@ -1,4 +1,12 @@
-export function placeInTheCenter(div1, div2){
-    const {clientTop} = div1
-    console.log(div1, div2);
-  }
+export function placeIntoCenter(div1, div2) {
+
+  if (div1.offsetHeight < div2.offsetHeight && div1.offsetWidth < div2.offsetWidth)
+    [div1, div2] = [div2, div1];
+
+  div2.style.position = 'absolute';
+  div2.style.right = '';
+  div2.style.bottom = '';
+  div2.style.top = `${div1.offsetTop + div1.offsetHeight / 2 - div2.offsetHeight / 2}px`;
+  div2.style.left = `${div1.offsetLeft + div1.offsetWidth / 2 - div2.offsetWidth / 2}px`;
+
+}
